7351e47bb5d9e52a71ff880c85696b9a83be4ee0
jvjungles <jvjungles@gmail.com>
2023-05-19 01:00:09 -0300

    init
    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -11,6 +12,7 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    MatSlideToggleModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
     ])
